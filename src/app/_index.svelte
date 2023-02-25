98f8c8d209d39bc393858d5b647413b27c5f3b16
Tiziano Perego <69322987+T1xx1@users.noreply.github.com>
2023-02-25 12:26:19 +0100

    Editor dimensions on mobile screen

--- src/app/_index.svelte
+++ src/app/_index.svelte
@@ -5,15 +5,3 @@
 
 <Title />
 <Main />
-
-<style lang="scss">
-   :global(.block) {
-      height: 100%;
-      width: clamp(500px, 50%, 50%);
-
-      @media (max-width: 1000px) {
-         height: 500px;
-         width: 99%;
-      }
-   }
-</style>
