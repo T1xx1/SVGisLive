95ed0126712877c4741bccb6e0cebaf110eeb862
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-03-11 22:45:47 +0100

    Formatter

--- src/app/main/editor/_index.svelte
+++ src/app/main/editor/_index.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
    import Editor from './editor';
-   import onkeyup from './onkeyup';
 
    export let svg;
 
-   let textarea;
-
    let editor = new Editor();
-
-   setTimeout(() => {
-      editor.e = textarea;
-   });
 </script>
 
 <textarea
    class="block"
    spellcheck="false"
-   bind:this={textarea}
+   bind:this={editor.e}
    bind:value={svg}
-   on:keyup={() => onkeyup(editor)}
+   on:keyup={editor.formatter}
 />
 
 <style>
--- src/app/main/editor/editor.ts
+++ src/app/main/editor/editor.ts
@@ -5,17 +5,33 @@ export default class Editor {
       this.e = e;
    }
 
-   replace(o: string, n: string) {
+   match(regex: RegExp) {
+      return this.e.value.match(regex);
+   }
+   replace(o: RegExp, n: string) {
       this.e.value = this.e.value.replace(o, n);
    }
 
-   moveCaret(n: number) {
-      this.setCaretPos(this.getCaretPos() + n);
-   }
-   setCaretPos(start: number, end = start) {
-      this.e.setSelectionRange(start, end);
-   }
-   getCaretPos() {
-      return this.e.selectionStart;
-   }
+   caret = {
+      setPos: (start: number, end = start) => {
+         this.e.setSelectionRange(start, end);
+      },
+      getPos: () => {
+         return this.e.selectionStart;
+      },
+
+      move(n: number) {
+         this.setPos(this.getPos() + n);
+      },
+   };
+
+   formatter = () => {
+      this.replace(/"/, "'");
+      if (this.match(/\w\/>/)) this.replace(/\w\/>/, ' />');
+
+      if (this.match(/></)) {
+         this.replace(/></g, '>\n<');
+         this.caret.move(1);
+      }
+   };
 }
