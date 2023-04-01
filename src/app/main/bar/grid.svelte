115a5cdb9ce0926e3b3177d2ed4ef1ed1f779460
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-04-01 19:09:15 +0200

    Grid btn

---
+++ src/app/main/bar/grid.svelte
@@ -0,0 +1,12 @@
+<script>
+   import BarBtn from '../../../components/barBtn.svelte';
+
+   import grid from '../../../assets/svg/grid.svg';
+</script>
+
+<BarBtn
+   click={() => {}}
+   type="toggle"
+   icon={grid}
+   title="Enable/disable grid"
+/>
